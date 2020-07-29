e213901a8b7fc6a995ff20de23e68592f79228f3
toribreeana <tori.bree@outlook.com>
2020-07-29 10:34:40 -0400

    finished

--- src/main/java/com/stayready/Problem02.java
+++ src/main/java/com/stayready/Problem02.java
@@ -1,4 +1,29 @@
 package com.stayready;
 
+import java.util.Arrays;
+
 public class Problem02 {
+
+    public String biggestBoss(int[] array) {
+        String result = "Length of the longest contiguous subarray is ";
+        Arrays.sort(array);
+        int count = 1;
+        int maxCount = 0;
+
+        for (int i = 0; i < array.length - 1; i++) {
+            if (array[i + 1] == array[i] + 1) {
+                count += 1;
+            } else {
+                if (maxCount < count) {
+                    maxCount = count;
+                    count = 1;
+                }
+            }
+        }
+       // maxCount = (maxCount < count)?count:maxCount;
+        if(maxCount < count){
+            maxCount = count;
+        }
+        return result + maxCount;
+    }
 }
--- src/test/java/com/stayready/Problem02Test.java
+++ src/test/java/com/stayready/Problem02Test.java
@@ -1,4 +1,57 @@
 package com.stayready;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
+
 public class Problem02Test {
+
+    @Before
+    public void setUp() {
+
+    }
+
+    @Test
+    public void input01() {
+        //Given
+        int array[] = {10, 12, 11};
+        Problem02 problem02 = new Problem02();
+
+        //When
+        String expected = "Length of the longest contiguous subarray is 3";
+        String actual = problem02.biggestBoss(array);
+
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void input02() {
+        //Given
+        int array[] = {14, 12, 11, 20};
+        Problem02 problem02 = new Problem02();
+        //When
+        String expected = "Length of the longest contiguous subarray is 2";
+        String actual = problem02.biggestBoss(array);
+
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void input03() {
+        //Given
+        int array[] = {1, 56, 58, 57, 90, 92, 94, 93, 91, 45};
+        Problem02 problem02 = new Problem02();
+
+        //When
+        String expected = "Length of the longest contiguous subarray is 5";
+        String actual = problem02.biggestBoss(array);
+
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+
 }
